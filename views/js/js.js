67dd8e9edd5bf185e4fbb400e542e39992b3900b
Johanren <jfrengifo04@misena.edu.co>
2024-04-27 17:43:41 -0500

    solucion

--- views/js/js.js
+++ views/js/js.js
@@ -1,5 +1,5 @@
 //Imprimir Pedido Cocina
-document.addEventListener('DOMContentLoaded', function () {
+/*document.addEventListener('DOMContentLoaded', function () {
 	// Este código se ejecutará cuando la página haya cargado completamente
 	var boton = document.getElementById('btnImprimir');
 
@@ -7,8 +7,15 @@ document.addEventListener('DOMContentLoaded', function () {
 	setTimeout(function () {
 		boton.click(); // Desencadenamos el evento de clic en el botón
 	}, 3000); // 3000 milisegundos = 3 segundos
-});
+});*/
 
+function hacerClic() {
+	document.getElementById('btnImprimir').click();
+	console.log("hizo clic");
+}
+window.onload = function () {
+	setInterval(hacerClic, 3000);
+};
 //Autocomplete
 $("#proeevedor").autocomplete({
 	source: function (request, response) {
@@ -313,7 +320,7 @@ $(document).ready(function () {
 $(document).ready(function () {
 	var index = 2;
 	$("#agregarProducto").click(function () {
-		$("#producto").append('<tr><td><input type="hidden" id="id_producto_'+index+'" name="id_producto[]"><input type="text" class="form-control" id="codigo_'+index+'" name="codigo[]"></td><td><input type="text" class="form-control nombrePro" id="producto_'+index+'" name="nombre[]"></td><td><input type="text" class="form-control" id="precio_'+index+'" name="precio[]"></td><td><input type="hidden" id="cantidad_'+index+'" name="cant[]"><input type="text" class="form-control" name="cantidad[]"></td><td><input type="hidden" class="form-control" name="id_categoria[]"id="id_categoria_'+index+'"><input type="text" class="form-control categoria"name="" id="categoria_'+index+'"></td><td><input type="hidden" class="form-control" name="id_medida[]"id="id_medida_'+index+'"><input type="text" class="form-control medida" name=""id="medida_'+index+'"></td><?phpif ($_SESSION["rol"] == "Administrador") {?><td><input type="hidden" class="form-control " name="id_local[]"id="id_local_'+index+'"><input type="text" class="form-control nom_local"id="local_'+index+'"></td><?php}?></tr>');
+		$("#producto").append('<tr><td><input type="hidden" id="id_producto_' + index + '" name="id_producto[]"><input type="text" class="form-control" id="codigo_' + index + '" name="codigo[]"></td><td><input type="text" class="form-control nombrePro" id="producto_' + index + '" name="nombre[]"></td><td><input type="text" class="form-control" id="precio_' + index + '" name="precio[]"></td><td><input type="hidden" id="cantidad_' + index + '" name="cant[]"><input type="text" class="form-control" name="cantidad[]"></td><td><input type="hidden" class="form-control" name="id_categoria[]"id="id_categoria_' + index + '"><input type="text" class="form-control categoria"name="" id="categoria_' + index + '"></td><td><input type="hidden" class="form-control" name="id_medida[]"id="id_medida_' + index + '"><input type="text" class="form-control medida" name=""id="medida_' + index + '"></td><?phpif ($_SESSION["rol"] == "Administrador") {?><td><input type="hidden" class="form-control " name="id_local[]"id="id_local_' + index + '"><input type="text" class="form-control nom_local"id="local_' + index + '"></td><?php}?></tr>');
 		index++;
 	});
 });
@@ -323,7 +330,7 @@ $(document).ready(function () {
 $(document).ready(function () {
 	var index = 2;
 	$("#agregarIngrediente").click(function () {
-		$("#ingrediente").append('<tr><td><input type="hidden" id="id_ingre_'+index+'" name="id_ingre[]"><input type="text" class="form-control ingre" id="ingre_'+index+'" name="nom_ingre[]"></td><td><input type="hidden" name="cantidadIngre[]" id="cantidad_'+index+'"><input type="text" class="form-control" name="cant[]"></td><td><input type="hidden" class="form-control" name="id_medida[]"id="id_medida_'+index+'"><input type="text" class="form-control medida" name=""id="medida_'+index+'"></td>phpif ($_SESSION["rol"] == "Administrador") {?><td><input type="hidden" class="form-control " name="id_local[]"id="id_local_'+index+'"><input type="text" class="form-control nom_local"id="local_'+index+'"></td><?php}?></tr>');
+		$("#ingrediente").append('<tr><td><input type="hidden" id="id_ingre_' + index + '" name="id_ingre[]"><input type="text" class="form-control ingre" id="ingre_' + index + '" name="nom_ingre[]"></td><td><input type="hidden" name="cantidadIngre[]" id="cantidad_' + index + '"><input type="text" class="form-control" name="cant[]"></td><td><input type="hidden" class="form-control" name="id_medida[]"id="id_medida_' + index + '"><input type="text" class="form-control medida" name=""id="medida_' + index + '"></td>phpif ($_SESSION["rol"] == "Administrador") {?><td><input type="hidden" class="form-control " name="id_local[]"id="id_local_' + index + '"><input type="text" class="form-control nom_local"id="local_' + index + '"></td><?php}?></tr>');
 		index++;
 	});
 });

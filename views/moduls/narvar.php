ff9fcb8097cdced31909adb30b1415a6f3303758
Johanren <jfrengifo04@misena.edu.co>
2024-04-29 15:02:02 -0500

    logos

--- views/moduls/narvar.php
+++ views/moduls/narvar.php
@@ -10,7 +10,7 @@ $res = $listar->consultarUsuarioPerfil();
         <div class="sidebar-brand-icon rotate-n-15">
             <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Junior Pizza <sup>2</sup></div>
+        <div class="sidebar-brand-text mx-3">Sazón Del Chef</div>
     </a>
 
     <!-- Divider -->

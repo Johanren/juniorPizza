d5f9fed44940fcc6d50cd201f8fe7feb61edb7e9
Johanren <jfrengifo04@misena.edu.co>
2024-05-03 19:29:21 -0500

    actualizacion

--- views/actualizar.php
+++ views/actualizar.php
@@ -1,19 +1,12 @@
 <?php
 require_once '../models/conexion.php';
-foreach ($_POST as $key => $value) {
-    
-    $sql = "UPDATE funiones SET estado = ? WHERE nombre_campo = ?";
-    $conn = new Conexion();
-    $stms = $conn->conectar()->prepare($sql);
-    $stms->bindParam(1, $value, PDO::PARAM_STR);
-    $stms->bindParam(2, $key, PDO::PARAM_STR);
-    try {
-        if ($stms->execute()) {
-            return true;
-        } else {
-            return false;
-        }
-    } catch (PDOException $e) {
-        print_r($e->getMessage());
-    }
+require_once '../models/modeloFuncion.php';
+
+foreach ($_POST as $nombre_campo => $valor) {
+    // Verificar si el checkbox está marcado
+
+    // Actualizar el estado en la base de datos
+    $actualizar = new ModeloFuncion();
+    $res = $actualizar->actualizarFuncion($valor, $nombre_campo);
 }
+
